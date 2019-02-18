a5fdd286c899daae8002c8df6f9e2e5c1c6fc19e
MoRdreD (Vadim Vozniuk) <47083343+alampiler@users.noreply.github.com>
2019-02-18 16:09:37 +0200

    Add big resolution styles and some js scripts

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
 
+/*Задаем класс для скрытого блока библиотеки*/
 $('.video-js').addClass('hover');
 
+/*Задаем ховер эффект и нажатие для видео и формы при разных разрешениях екрана*/
 if($(window).width() > 1024){
   $('.hover').hover(function () {
     $('.hover').removeClass('active');
@@ -18,6 +20,41 @@ else{
   $('.hover').removeClass('active');
 }
 
+/*Плавный переход по якроям ссылок*/
+  $("a[href*='#']").on("click", function(e){
+    let anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+  });
+
+  $(".order").on("click", function(e){
+    let anchor = $("#presentation");
+    $('html, body').stop().animate({
+      scrollTop: $(anchor).offset().top
+    }, 777);
+    e.preventDefault();
+    return false;
+  });
+
+/*Скролер на верх сайта*/
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1400) {
+      $('.scrollUp').fadeIn();
+    } else {
+      $('.scrollUp').fadeOut();
+    }
+  });
+
+  $('.scrollUp').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+  });
+
+
+ /*Меню для моб. екранов*/
 $('#mob_btn').click(function () {
   $('.mobile_navbar').addClass('active');
 });
@@ -26,7 +63,7 @@ $('#mob_btn').click(function () {
     $('.mobile_navbar').removeClass('active');
   });
 
-
+/*Кастомные свойства фильтров библиотеки ion.rangeSlider*/
   $("#filer_price").ionRangeSlider({
     type: "double",
     grid: false,
